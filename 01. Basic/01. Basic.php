5ee47eef0b37d4306eeb34fa74688429a674b8f0
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2024-02-17 18:56:33 +0600

    Add files via upload

---
+++ 01. Basic/01. Basic.php
@@ -0,0 +1,22 @@
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/* This is a
+multi-line comment */
+
+/* In PHP, keywords (if, else, while, echo, etc.) and 
+classes, functions, and user-defined functions are not case-sensitive. */
+ECHO "Yash Rohan";
+echo "Virat Kholi";
+EcHo "Messi";
+
+
+// All variable names are case-sensitive!
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+
+?>
