2172624af05067ec6b78dd54ddd1161882fe8711
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2024-03-05 08:10:55 +0600

    Add files via upload

---
+++ 13. SuperGlobals/06.demo_request.php
@@ -0,0 +1,6 @@
+<?php
+
+$name = $_POST['fname'];
+echo $name;
+
+?>
---
+++ 13. SuperGlobals/09.welcome_get.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+<!--In this action file we can use the $_GET variable to collect the value of the input fields-->
+Welcome <?php echo $_GET["name"]; ?><br>
+Email address is: <?php echo $_GET["email"]; ?>
+
+</body>
+</html>
